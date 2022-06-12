e4dd4b0b70208483b1232b51aca29df1b662080b
Archit Jain <arcyjain2002@gmail.com>
2022-06-13 00:00:20 +0530

    navbar skeleton ready

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,30 +2,6 @@ import React from "react";
 
 export default function Navbar() {
   return (
-    // <div>
-    //   <nav className="navbar" style={{ backgroundColor: "#41B3A3" }}>
-    //     <ul className="nav">
-    //       <li className="nav-item">
-    //         <a className="nav-link active" aria-current="page" href="#">
-    //           Feeds
-    //         </a>
-    //       </li>
-    //       <li className="nav-item">
-    //         <a className="nav-link" href="#">
-    //           Upload
-    //         </a>
-    //       </li>
-    //     </ul>
-    //     <ul className="navbar-nav ms-auto">
-    //       <li className="nav-item">
-    //         <a className="nav-link" href="#">
-    //           History
-    //         </a>
-    //       </li>
-    //     </ul>
-    //   </nav>
-    // </div>
-
     <nav className="navbar navbar-expand-md navbar-dark bg-primary">
       <div className="container-fluid">
         <a className="navbar-brand abs" href="#" style={{ fontSize: "20px" }}>
