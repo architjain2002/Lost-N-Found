1e31e76e78aadacbe4ac684f5807d91a527164ba
Archit Jain <arcyjain2002@gmail.com>
2022-06-12 23:55:05 +0530

    updated navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -28,7 +28,7 @@ export default function Navbar() {
 
     <nav className="navbar navbar-expand-md navbar-dark bg-primary">
       <div className="container-fluid">
-        <a className="navbar-brand abs" href="#">
+        <a className="navbar-brand abs" href="#" style={{ fontSize: "20px" }}>
           L&F
         </a>
         <button
@@ -41,25 +41,48 @@ export default function Navbar() {
         <div className="navbar-collapse collapse" id="collapseNavbar">
           <ul className="navbar-nav">
             <li className="nav-item active">
-              <a className="nav-link" href="#">
+              <a className="nav-link" href="#" style={{ fontSize: "20px" }}>
                 Feeds
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#myAlert" data-bs-toggle="collapse">
+              <a
+                className="nav-link"
+                href="#"
+                data-bs-toggle="collapse"
+                style={{ fontSize: "20px" }}>
                 Upload
               </a>
             </li>
           </ul>
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
+          <ul className="navbar-nav ms-auto ">
+            <li className="nav-item p-2">
               <a
                 className="nav-link"
-                href=""
+                href="#"
                 data-bs-target="#myModal"
                 data-bs-toggle="modal">
                 <i
                   className="bi bi-clock-history"
+                  style={{ fontSize: "25px" }}></i>
+              </a>
+            </li>
+            <li className="nav-item p-2">
+              <button
+                type="button"
+                className="btn btn-outline-light"
+                style={{ fontSize: "20px" }}>
+                Login
+              </button>
+            </li>
+            <li className="nav-item p-2">
+              <a
+                className="nav-link"
+                href="#"
+                data-bs-target="#myModal"
+                data-bs-toggle="modal">
+                <i
+                  className="bi bi-person-circle"
                   style={{ fontSize: "25px" }}></i>
               </a>
             </li>
