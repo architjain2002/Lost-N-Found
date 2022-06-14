53ef6b346244ffbce4136ec493d11d2e17104881
Archit Jain <arcyjain2002@gmail.com>
2022-06-14 18:23:16 +0530

    clear state when update ImageFile

--- frontend/src/components/Uploads.js
+++ frontend/src/components/Uploads.js
@@ -3,17 +3,17 @@ import React, { useState, useEffect } from "react";
 export default function Uploads() {
   const [ImageFile, setImageFile] = useState([]);
 
+  // stores the imagefiles into the ImageFile state
   function imageFileHandler(event) {
     const fileList = event.target.files;
     console.log(fileList);
+    setImageFile([]); // whenever changes happen again we delete all the image from the curr state.
     for (let index = 0; index < fileList.length; index++) {
       const file = fileList[index];
       setImageFile((prevState) => {
         return [...prevState, file];
       });
     }
-
-    console.log(ImageFile);
   }
 
   useEffect(() => {
