5c881f85f42b1f688588af141b0b1bc84ee9f3ba
Archit Jain <arcyjain2002@gmail.com>
2022-06-14 18:16:32 +0530

    images array created and stored

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 import Navbar from "./components/Navbar.js";
 import Leaderboard from "./components/Leaderboard.js";
+import Uploads from "./components/Uploads.js";
 
 function App() {
   return (
     <>
       <Navbar />
       <Leaderboard />
+      <Uploads />
     </>
   );
 }
--- frontend/src/components/Uploads.js
+++ frontend/src/components/Uploads.js
@@ -1,5 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 export default function Uploads() {
-  return <div>Uploads</div>;
+  const [ImageFile, setImageFile] = useState([]);
+
+  function imageFileHandler(event) {
+    const fileList = event.target.files;
+    console.log(fileList);
+    for (let index = 0; index < fileList.length; index++) {
+      const file = fileList[index];
+      setImageFile((prevState) => {
+        return [...prevState, file];
+      });
+    }
+
+    console.log(ImageFile);
+  }
+
+  useEffect(() => {
+    console.log(ImageFile);
+  }, [ImageFile]);
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept="image/png, image/jpeg"
+        id="InputImage"
+        onChange={imageFileHandler}
+        multiple
+      />
+
+      {/* <div>
+        {ImageFile.map((image) => (
+          <p>{image}</p>
+        ))}
+      </div> */}
+    </div>
+  );
 }
