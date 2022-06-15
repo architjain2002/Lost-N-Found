540e51d774b52efdb483d25a9241e0c83f15a524
Archit Jain <arcyjain2002@gmail.com>
2022-06-15 22:53:25 +0530

    added paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,22 @@
 import Navbar from "./components/Navbar.js";
 import Leaderboard from "./components/Leaderboard.js";
 import TextBox from "./components/TextBox.js";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 function App() {
   return (
     <>
-      <Navbar />
-      <Leaderboard />
-      <TextBox />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path="/feeds">
+            <Feeds />
+          </Route>
+
+          <Route path="/upload">
+            <Upload />
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
